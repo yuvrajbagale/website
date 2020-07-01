17e504308b493b179992bbcc21e0e50e31991f84
Kevin Miller <keveemiller@gmail.com>
2020-07-01 15:40:07 -0600

    feat: Add legend

--- src/components/pages/homepage/us-map.js
+++ src/components/pages/homepage/us-map.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import React, { useState, useMemo } from 'react'
-import { useStaticQuery, graphql, Link, navigate } from 'gatsby'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import { geoPath, geoMercator } from 'd3-geo'
 import slugify from 'slugify'
 import classnames from 'classnames'
@@ -17,19 +17,16 @@ const margin = {
 }
 
 const limit = number => {
-  if (number <= 500) {
-    return 500
+  if (number <= 1000) {
+    return 'Low'
   }
   if (number <= 2000) {
-    return 1000
+    return 'Medium'
   }
-  if (number <= 5000) {
-    return 2000
-  }
-  return 5000
+  return 'High'
 }
 
-const State = ({ feature, path, setHover, isHovered = false }) => {
+const State = ({ feature, path, setActive, isActive = false }) => {
   const levelClass =
     usMapStyles[`level${limit(feature.properties.stateInfo.sevenDayPositive)}`]
 
@@ -39,23 +36,21 @@ const State = ({ feature, path, setHover, isHovered = false }) => {
       d={path(feature)}
       className={classnames(
         usMapStyles.state,
-        isHovered && usMapStyles.hovered,
+        isActive && usMapStyles.active,
         levelClass,
       )}
-      onMouseEnter={() => {
-        if (isHovered) {
-          return false
+      onClick={event => {
+        event.preventDefault()
+        if (isActive) {
+          return setActive(false)
         }
-        return setHover(feature)
-      }}
-      onClick={() => {
-        navigate(feature.properties.link)
+        return setActive(feature)
       }}
     />
   )
 }
 
-const Label = ({ feature, path }) => {
+const Label = ({ feature, setActive, path }) => {
   const centroid = path.centroid(feature)
   const levelClass =
     usMapStyles[`text${limit(feature.properties.stateInfo.sevenDayPositive)}`]
@@ -69,9 +64,7 @@ const Label = ({ feature, path }) => {
           usMapStyles.stateLabel,
           levelClass,
         )}
-        onClick={() => {
-          navigate(feature.properties.stateInfo.link)
-        }}
+        onClick={() => setActive(feature)}
       >
         {feature.properties.stateInfo.state}
       </text>
@@ -79,9 +72,7 @@ const Label = ({ feature, path }) => {
         x={centroid[0] - 20}
         y={centroid[1] + 15}
         className={classnames(usMapStyles.label, levelClass)}
-        onClick={() => {
-          navigate(feature.properties.stateInfo.link)
-        }}
+        onClick={() => setActive(feature)}
       >
         {feature.properties.stateInfo.sevenDayPositive.toLocaleString()}
       </text>
@@ -105,7 +96,9 @@ const StateListStatistics = ({ title, states, level }) => {
             )}
           >
             <Link to={state.link}>
-              <div className={usMapStyles.name}>{state.state}</div>
+              <div className={usMapStyles.name} aria-hidden>
+                {state.state}
+              </div>
               <span className="a11y-only">{state.name}</span>
               <div className={usMapStyles.number}>
                 {state.sevenDayPositive.toLocaleString()}
@@ -121,29 +114,21 @@ const StateListStatistics = ({ title, states, level }) => {
 const StateList = ({ states }) => (
   <div className={usMapStyles.stateList}>
     <StateListStatistics
-      title="Over 5,000 cases"
-      level={5000}
-      states={states.filter(state => state.sevenDayPositive > 5000)}
-    />
-    <StateListStatistics
-      title="2,000-5,000 cases"
-      level={2000}
-      states={states.filter(
-        state =>
-          state.sevenDayPositive >= 2000 && state.sevenDayPositive < 5000,
-      )}
+      title="Over 2,000 cases"
+      level="High"
+      states={states.filter(state => state.sevenDayPositive > 2000)}
     />
     <StateListStatistics
       title="1,000-2,000 cases"
-      level={1000}
+      level="Medium"
       states={states.filter(
         state =>
           state.sevenDayPositive >= 1000 && state.sevenDayPositive < 2000,
       )}
     />
     <StateListStatistics
-      title="Under 1,000 cases"
-      level={500}
+      title="Below 1,000 cases"
+      level="Low"
       states={states.filter(state => state.sevenDayPositive < 1000)}
     />
   </div>
@@ -154,8 +139,96 @@ const getAverage = (history, field) =>
     history.reduce((total, item) => total + item[field], 0) / history.length,
   )
 
+const Tooltip = ({ feature, path }) => {
+  const bounds = path.bounds(feature)
+
+  return (
+    <svg x={bounds[1][0]} y={bounds[1][1]}>
+      <rect x={0} y={0} className={usMapStyles.tooltip}>
+        <text>{feature.properties.stateInfo.name}</text>
+      </rect>
+    </svg>
+  )
+}
+
+const MapLegendItem = ({ title, className }) => (
+  <div className={usMapStyles.item}>
+    <div className={classnames(usMapStyles.bar, className)} />
+    {title}
+  </div>
+)
+
+const MapLegend = () => (
+  <div className={usMapStyles.legend}>
+    <MapLegendItem title="Below 1,000 cases" className={usMapStyles.low} />
+    <MapLegendItem title="1,000 to 2,000" className={usMapStyles.medium} />
+    <MapLegendItem title="Above 2,000 cases" className={usMapStyles.high} />
+  </div>
+)
+
+const Map = () => {
+  const [activeState, setActiveState] = useState(false)
+  const path = useMemo(() => {
+    const projection = geoMercator().fitExtent(
+      [
+        [margin.left, margin.top],
+        [mapWidth - margin.right, mapHeight - margin.bottom],
+      ],
+      stateShapes,
+    )
+    return geoPath().projection(projection)
+  }, [mapWidth, mapHeight])
+
+  return (
+    <>
+      <svg
+        className={usMapStyles.map}
+        width={mapWidth}
+        height={mapHeight}
+        onMouseLeave={() => setActiveState(false)}
+        aria-hidden
+      >
+        <g>
+          {stateShapes.features.map(feature => (
+            <State
+              feature={feature}
+              path={path}
+              setActive={stateFeature => setActiveState(stateFeature)}
+            />
+          ))}
+        </g>
+        <g>
+          {stateShapes.features.map(feature => (
+            <Label
+              feature={feature}
+              path={path}
+              setActive={stateFeature => setActiveState(stateFeature)}
+            />
+          ))}
+        </g>
+
+        {activeState && (
+          <g>
+            <State
+              feature={activeState}
+              path={path}
+              setActive={() => setActiveState(false)}
+              isActive
+            />
+            <Label
+              feature={activeState}
+              path={path}
+              setActive={() => setActiveState(false)}
+            />
+            <Tooltip feature={activeState} path={path} />
+          </g>
+        )}
+      </svg>
+      <MapLegend />
+    </>
+  )
+}
 export default () => {
-  const [hoveredState, setHoveredState] = useState(false)
   const data = useStaticQuery(graphql`
     {
       allCovidStateInfo {
@@ -197,57 +270,13 @@ export default () => {
     )
   })
 
-  const path = useMemo(() => {
-    const projection = geoMercator().fitExtent(
-      [
-        [margin.left, margin.top],
-        [mapWidth - margin.right, mapHeight - margin.bottom],
-      ],
-      stateShapes,
-    )
-    return geoPath().projection(projection)
-  }, [mapWidth, mapHeight])
-
   return (
     <div className={usMapStyles.mapWrapper}>
       <h2 className={usMapStyles.mapHeading}>
         New COVID-19 cases by state/territory
         <div>Seven-day rolling average</div>
       </h2>
-      <svg
-        className={usMapStyles.map}
-        width={mapWidth}
-        height={mapHeight}
-        onMouseLeave={() => setHoveredState(false)}
-        aria-hidden
-      >
-        <g>
-          {stateShapes.features.map(feature => (
-            <State
-              feature={feature}
-              path={path}
-              setHover={stateFeature => setHoveredState(stateFeature)}
-            />
-          ))}
-        </g>
-        <g>
-          {stateShapes.features.map(feature => (
-            <Label feature={feature} path={path} />
-          ))}
-        </g>
-
-        {hoveredState && (
-          <g>
-            <State
-              feature={hoveredState}
-              path={path}
-              setHover={state => setHoveredState(state)}
-              isHovered
-            />
-            <Label feature={hoveredState} path={path} />
-          </g>
-        )}
-      </svg>
+      <Map />
       <StateList states={states} />
       <p>
         We compile data from official, public state and territory data. This
