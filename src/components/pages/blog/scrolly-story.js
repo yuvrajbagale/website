74abb12d625a96e652d31582381770fb6851c672
laurakurtzberg <laura.kurtzberg@gmail.com>
2020-09-29 13:46:55 -0700

    first pass at what a ScrollyStory component might look like

--- src/components/pages/blog/scrolly-story.js
+++ src/components/pages/blog/scrolly-story.js
@@ -1,11 +1,59 @@
 import React from 'react'
-import ImageCredit from '~components/common/image-credit'
 
-const ScrollyStory = ({ imageSource, imageAlt, caption, containerStyle }) => (
-  <div className={containerStyle}>
-    <img src={imageSource} alt={imageAlt} />
-    <ImageCredit>{caption}</ImageCredit>
-  </div>
-)
+/*
+ * I'm assuming that the incoming data looks something like this:
+ * scrollyStoryProps {
+ *    stepOne: {
+ *       copy: 'this is the first piece of text',
+ *       image: imageSourceVariable1,
+ *       altText: 'alt for first image',
+ *     },
+ *     stepTwo: {
+ *       copy: 'this is the second piece of text',
+ *       image: imageSourceVariable2,
+ *       altText: 'alt for second image',
+ *     },
+ *     stepThree: {
+ *       copy: 'this is the third piece of text',
+ *       image: imageSourceVariable3,
+ *       altText: 'alt for third image',
+ *     }
+ * }
+ *
+ */
+
+const tempData = {
+  stepOne: {
+    copy: 'this is the first piece of text',
+    image: '~images/experiments/pooled_testing_00.jpg',
+    altText: 'alt for first image',
+  },
+  stepTwo: {
+    copy: 'this is the second piece of text',
+    image: '~images/experiments/pooled_testing_01.jpg',
+    altText: 'alt for second image',
+  },
+  stepThree: {
+    copy: 'this is the third piece of text',
+    image: '~images/experiments/pooled_testing_02.jpg',
+    altText: 'alt for third image',
+  },
+}
+
+const ScrollyStory = ({ scrollyStoryProps = tempData }) => {
+  const container = document.getElementById('container')
+
+  return (
+    <div className="scrollyContainer">
+      {Object.keys(scrollyStoryProps).map(key => (
+        <img
+          className="scrollyImage"
+          src={scrollyStoryProps[key].image}
+          alt={scrollyStoryProps[key].altText}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default ScrollyStory
